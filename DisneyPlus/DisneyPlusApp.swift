d69bf66d85892bb02b0a1a984db1dc532035e6dc
이기완 <kiwan.lee@waiker.ai>
2023-01-13 16:07:39 +0900

    Initial Commit

---
+++ DisneyPlus/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusApp.swift
+//  DisneyPlus
+//
+//  Created by 이기완 on 2023/01/13.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
